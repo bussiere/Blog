9ccb6a89a696720c32ffdc77609d12521bb788dd
bussiere <bussiere@gmail.com>
2012-12-03 21:16:33 +0100

    test

--- extra/README.md
+++ extra/README.md
@@ -1,8 +1,8 @@
 Date: 2012-11-26
 Title: Readme
 Category: Blog
-Tags: fez,decouverte,debut
-[0]: http://raw.github.com/bussiere/fez_odissey/gh-pages/static/images/_04.jpg  "Grande Version"
+Tags: Vampirette,Python,Readme,Geek
+[0]: http://raw.github.com/bussiere/Blog/gh-pages/static/images/_00.jpg  "Grande Version"
 
-Ceci est un petit blog sur fez pour montrer nos decouvertes et notre aventure.
-[![Ou ai-je atterri ?](http://raw.github.com/bussiere/fez_odissey/gh-pages/static/images/_04_thumb.jpg)] [0] 
+Ce blog a pour vocation de parler de python, de trucs geeks et divers ou curieux, et de femmes.
+[![Vampirette](http://raw.github.com/bussiere/Blog/gh-pages/static/images/_00_thumb.jpg)] [0] 
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = u'Bussiere & Camille'
-SITENAME = u'A Fez Odyssey'
-SITEURL = 'http://bussiere.github.com/fez_odissey'
+AUTHOR = u'Bussiere'
+SITENAME = u'Du python , de la tech qui tache et des femmes'
+SITEURL = 'http://bussiere.github.com/Blog'
 
 TIMEZONE = 'Europe/Paris'
-FEED_DOMAIN = 'http://bussiere.github.com/fez_odissey'
+FEED_DOMAIN = 'http://bussiere.github.com/Blog'
 DEFAULT_LANG = u'en'
 
 TEMPLATE_PAGES = "./"
@@ -14,11 +14,9 @@ TEMPLATE_PAGES = "./"
 # Blogroll
 LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+          ('Jinja2', 'http://jinja.pocoo.org'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Bussiere', 'https://github.com/bussiere'),)
 
 DEFAULT_PAGINATION = 10
