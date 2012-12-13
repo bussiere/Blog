0145267c3bc700abbef9447cfdbd53a593a69462
bussiere <bussiere@gmail.com>
2012-12-13 14:23:17 +0100

    mise jour de la conf

--- extra/pelicanconf.py
+++ extra/pelicanconf.py
@@ -14,7 +14,6 @@ LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('Python.org', 'http://python.org'),
           ('Jinja2', 'http://jinja.pocoo.org'),)
 # Social widget
-SOCIAL = (('Bussiere', 'https://github.com/bussiere'),
-          ('Camille', '#'),)
+SOCIAL = (('Bussiere', 'https://github.com/bussiere'),)
 
 DEFAULT_PAGINATION = 10
